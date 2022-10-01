8e6c3dc7693c1bfe92d5d5bcf802c9e45b56be2c
trannhat <trannhat01052001@gmail.com>
2022-10-01 15:33:38 +0700

    up

---
+++ function.js
@@ -0,0 +1,13 @@
+// khoi tao cac thong so
+let btn1 = document.querySelector('#btn1');
+let img = document.querySelector('img');
+let btn2 = document.querySelector('#btn2');
+
+// functions nut bam
+btn1.addEventListener('click', ()=>{
+    img.src = 'denon1.gif'; 
+})
+
+btn2.addEventListener('click', ()=>{
+    img.src = 'den1.png';
+})
